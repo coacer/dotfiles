cf72313468c36e719f250e687da8373f2cd9e45e
coacer <coacer728h23@icloud.com>
2020-03-25 18:35:01 +0900

    install.sh作成

---
+++ install.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# ==== シンボリックリンク ====
+rm ~/.zshrc;
+rm ~/.bash_profile;
+rm ~/.bashrc;
+ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc;
+ln -s ~/dotfiles/zsh/.zsh.d ~/.zsh.d;
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile;
+ln -s ~/dotfiles/.bashrc ~/.bashrc;
+ln -s ~/dotfiles/.emacs.d ~/.emacs.d;
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig;
+ln -s ~/dotfiles/.tigrc ~/.tigrc;
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf;
+mkdir ~/.config;
+ln -s ~/dotfiles/nvim ~/.config/nvim;
+ln -s ~/dotfiles/.vimrc ~/.vimrc;
+
+# ==== Homebrew ====
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)";
+
+brew update;
+brew install vim neovim emacs fzf git hub docker lazydocker lazygit tmux tig \
+  tree autojump composer awscli rbenv pyenv pyenv-virtualenv nodenv;
+
+# 読み込み
+source ~/.zshrc
+
+# ==== Neovim ====
+# ==== ダイナミックプロバイダの設定 ====
+
+# == Python ==
+# 最新の安定版のバージョンをインストール
+python2=$(pyenv install -l | grep -v '[a-zA-Z]' | grep -e '\s2\.?*' | tail -1);
+python3=$(pyenv install -l | grep -v '[a-zA-Z]' | grep -e '\s3\.?*' | tail -1);
+pyenv install $python2;
+pyenv install $python3;
+pyenv rehash;
+# インストールしたバージョンでvirtualenvを設定
+pyenv virtualenv $python2 py2neovim;
+pyenv virtualenv $python3 py3neovim;
+# それぞれのvirtualenv内でneovimをインストール
+pyenv activate py2neovim;
+pip install neovim;
+pyenv activate py3neovim;
+pip install neovim;
+
+
+# == Ruby ==
+# 最新の安定版のバージョンをインストール
+ruby=$(rbenv install -l | grep -v '[a-zA-Z]' | grep -e '^2\.?*' | tail -1);
+rbenv install $ruby;
+rbenv rehash;
+rbenv global $ruby;
+gem install neovim;
+
+
+# == Node.js ==
+# 最新の安定版のバージョンをインストール
+node=$(nodenv install -l | grep -v '[a-zA-Z]' | grep -e '^12\.?*' | tail -1);
+nodenv install $node;
+nodenv rehash;
+nodenv global $node;
+npm install -g neovim;
+
