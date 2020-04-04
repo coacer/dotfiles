69b7ac7ccdd7cc3a05a13731b1f1d48f813a6c5c
coacer <coacer728h23@icloud.com>
2020-04-04 17:41:46 +0900

    Add .vimrc-ver2 and dein_install.sh

---
+++ dein_install.sh
@@ -0,0 +1,5 @@
+# ==== dein.vimインストール ====
+mkdir -p ~/.cache/dein;
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh;
+sh ./installer.sh ~/.cache/dein;
+rm installer.sh;
